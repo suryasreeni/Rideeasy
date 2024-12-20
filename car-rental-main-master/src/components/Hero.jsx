8b20cb9bb7548fe7993adbd9b3e3fbfc3961c424
suryasreeni <84901939+suryasreeni@users.noreply.github.com>
2024-12-20 21:43:20 +0530

    Update Hero.jsx

--- car-rental-main-master/src/components/Hero.jsx
+++ car-rental-main-master/src/components/Hero.jsx
@@ -39,7 +39,7 @@ function Hero() {
             <div className="hero-content__text">
               <h4>Plan your trip now</h4>
               <h1>
-                 <span>Welcome</span> to Suryaaaa's RideEasy Rental Services
+                 <span>Welcome</span> to Suryaaaa's RideEasy Rental Servicesmkdkfffffffffffffffffffffffffffffffffffffffffffffffffff
               </h1>
               <p>
                 Rent the car of your dreams. Unbeatable prices, unlimited miles,
